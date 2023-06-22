042b22f8838a9672f156ecb03aff34009dc2d83b
Layak Ali <93964740+Layak-Ali@users.noreply.github.com>
2023-06-22 23:14:03 +0530

    Create Card.jsx

---
+++ src/Card.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Card = ({movie}) => {
+    return(
+        <div className="movie" key={movie.imdbID}>
+            <div>
+                <p>{movie.Year}</p>
+            </div>
+
+            <div>
+                <img src={movie.Poster} alt={movie.Title}/>
+            </div>
+
+            <div>
+                <span>{movie.Type}</span>
+                <h3>{movie.Title}</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
